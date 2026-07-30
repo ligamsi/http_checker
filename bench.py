483ace6b9457744e6df0a451b3da68877fd29f23
ligamsi <galimov.ism@mail.ru>
2026-07-30 15:25:01 +0500

    min,max,avg

--- bench.py
+++ bench.py
@@ -23,11 +23,18 @@ hosts = args.hosts.split(",")
 
 for host in hosts:
     print(f"\nПроверяем {host}")
+    times = []
 
     for i in range(args.count):
         start = time.perf_counter()
         response = requests.get(host)
         finish = time.perf_counter()
         elapsed = finish - start
+        times.append(elapsed)
 
         print(f"Запрос {i + 1}: {response.status_code} ({elapsed:.3f} сек)")
+        print(f"\nСтатистика для {host}")
+
+        print(f"Min: {min(times):.3f}")
+        print(f"Max: {max(times):.3f}")
+        print(f"Avg: {sum(times) / len(times):.3f}")
