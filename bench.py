2af44d88da5fc1b2fd3d4762cafbc47a0d0d2ede
ligamsi <galimov.ism@mail.ru>
2026-07-30 17:46:11 +0500

    try except

--- bench.py
+++ bench.py
@@ -24,17 +24,33 @@ hosts = args.hosts.split(",")
 for host in hosts:
     print(f"\nПроверяем {host}")
     times = []
+    success = 0
+    failed = 0
+    errors = 0
 
     for i in range(args.count):
-        start = time.perf_counter()
-        response = requests.get(host)
-        finish = time.perf_counter()
-        elapsed = finish - start
-        times.append(elapsed)
+        try:
+            start = time.perf_counter()
+            response = requests.get(host)
+            finish = time.perf_counter()
+            elapsed = finish - start
+            times.append(elapsed)
 
-        print(f"Запрос {i + 1}: {response.status_code} ({elapsed:.3f} сек)")
-        print(f"\nСтатистика для {host}")
+            if response.status_code == 200:
+                success += 1
+            elif 400 <= response.status_code < 600:
+                failed += 1
 
-        print(f"Min: {min(times):.3f}")
-        print(f"Max: {max(times):.3f}")
-        print(f"Avg: {sum(times) / len(times):.3f}")
+            print(f"Запрос {i + 1}: {response.status_code} ({elapsed:.3f} сек)")
+        except requests.RequestException:
+            errors += 1
+            print(f"Запрос {i + 1}: Ошибка подключения")
+    print(f"\nСтатистика для {host}")
+    print(f"Success: {success}")
+    print(f"Failed: {failed}")
+    print(f"Errors: {errors}")
+
+    if times:
+        print(f"Min: {min(times):.3f} сек")
+        print(f"Max: {max(times):.3f} сек")
+        print(f"Avg: {sum(times) / len(times):.3f} сек")
